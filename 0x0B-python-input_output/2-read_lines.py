91e40608459f121bb0ccb2d9ba3cac802c3e527f
noahmohamed311 <noahmohamed311@gmail.com>
2022-07-05 17:55:40 +0300

    read lines

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Module to read lines from file"""
+
+
+def read_lines(filename="", nb_lines=0):
+    """Read `nb_lines` lines from `filename`
+    If `nb_lines` <= 0 read all lines. Otherwise, read up to `nb_lines`
+        or EOF, whichever comes first.
+    Args:
+        filename (str): name of file to read
+        nb_lines (int): maximum number of lines to read, or if <= 0, all lines
+    """
+    with open(filename, 'r', encoding='utf8') as f:
+        if nb_lines <= 0:
+            print(f.read(), end="")
+        else:
+            for line in f:
+                if nb_lines == 0:
+                    break
+                print(line, end="")
+                nb_lines -= 1
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+2-read_lines.py [unix] (02:59 01/01/1970)                              0,0-1 All
+"2-read_lines.py" [New]
+
