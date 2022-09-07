9857ed3ff270aa252b19dcbcdce7df663b653e96
noahmohamed311 <noahmohamed311@gmail.com>
2022-09-08 02:26:07 +0300

    rect

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+/**
+ * Represents a rectangle class.
+ */
+class Rectangle {
+  /**
+   * initializes an instance of the class
+   * @param {Number} w -- width
+   * @param {Number} h -- the value of the height
+  */
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
